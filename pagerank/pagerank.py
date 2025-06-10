df2f6813b608737e38ef3cd42313bfdab2d0f4dc
Lucas Pereira <lucas-m.p@hotmail.com>
2025-06-09 22:11:56 -0300

    finished pagerank

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -5,6 +5,7 @@ import sys
 
 DAMPING = 0.85
 SAMPLES = 10000
+CALC_THRESHOLD = 0.001
 
 
 def main():
@@ -57,15 +58,16 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
+    corpus_copy = corpus.copy()
     result = {}
-    if len(corpus[page].keys()) == 0:
-        corpus[page] = set(
-            p in corpus
+    if len(corpus[page]) == 0:
+        corpus_copy[page] = set(
+            p for p in corpus
         )
-    number_of_pages = len(corpus.keys())
-    for p in corpus:
-        pages_linked_to = corpus[p].keys()
-        num_links = (1 if page in pages_linked_to else 0) / len(pages_linked_to)
+    number_of_pages = len(corpus_copy)
+    pages_linked_to = corpus_copy[page]
+    for p in corpus_copy:
+        num_links = (1 if p in pages_linked_to else 0) / len(pages_linked_to)
         result[p] = (1-damping_factor)*(1/number_of_pages) + damping_factor*num_links
     return result
 
@@ -79,7 +81,32 @@ def sample_pagerank(corpus, damping_factor, n):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    result = {}
+    for p in corpus:
+        result[p] = 0
+    page_names = list(corpus)
+    random_page = page_names[int(random.random() * len(page_names))]
+    current_page = random_page
+    dp_model_dict = {}
+    for i in range(n):
+        dp_key = f'{current_page}-{damping_factor}'
+        model = {}
+        if dp_key in dp_model_dict:
+            model = dp_model_dict[dp_key]  
+        else:
+            model = transition_model(corpus, current_page, damping_factor)
+            dp_model_dict[dp_key] = model
+        random_value = random.random()
+        model_threshold = 0
+        for p in model:
+            model_threshold = model_threshold + model[p]
+            if random_value < model_threshold:
+                result[p] = result[p] + 1
+                current_page = p
+                break
+    for p in result:
+        result[p] = result[p]/n
+    return result
 
 
 def iterate_pagerank(corpus, damping_factor):
@@ -90,8 +117,39 @@ def iterate_pagerank(corpus, damping_factor):
     Return a dictionary where keys are page names, and values are
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
-    """
-    raise NotImplementedError
+    """    
+    max_retries = 10000000
+    result = {}
+    n = len(corpus)
+    received_links = {}
+    for p in corpus:
+        result[p] = 1/n
+        if len(corpus[p]) == 0:
+            corpus[p] = set(
+                l for l in corpus
+            )
+        for l in corpus[p]:
+            if l not in received_links:
+                received_links[l] = {}
+            received_links[l][p] = True
+
+    should_repeat = True
+    i = 0
+    while should_repeat:
+        should_repeat = False
+        base_pr = result.copy()
+        for p in result:
+            links = received_links[p]
+            sum_links = 0
+            for l in links:
+                sum_links = sum_links + base_pr[l]/len(corpus[l])
+            result[p] = (1-damping_factor)/n + damping_factor*sum_links
+            if abs(result[p] - base_pr[p]) > CALC_THRESHOLD:
+                should_repeat = True
+        i = i + 1
+        if i > max_retries:
+            raise f'max retry reached'
+    return result
 
 
 if __name__ == "__main__":
